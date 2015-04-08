21fb7fba1320830a6dc1d4bff26b4090f74ad3c5
Paul Agron <cout.flush@gmail.com>
2015-04-07 18:02:27 -0700

    Deleting StoryBoard

--- AcronymLookup/AcronymLookup/AppDelegate.m
+++ AcronymLookup/AcronymLookup/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -14,9 +15,13 @@
 
 @implementation AppDelegate
 
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
   // Override point for customization after application launch.
+  self.window.backgroundColor = [UIColor whiteColor];
+  self.window.rootViewController = [ViewController new];
+  [self.window makeKeyAndVisible];
   return YES;
 }
 
