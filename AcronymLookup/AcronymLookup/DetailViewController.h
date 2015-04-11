ecaed126480d66e7566dafacc34112e62cbad6e1
Paul Agron <cout.flush@gmail.com>
2015-04-11 02:01:26 -0700

    Mostly implementted, though far from ideal

---
+++ AcronymLookup/AcronymLookup/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  AcronymLookup
+//
+//  Created by Paul Agron on 4/10/15.
+//  Copyright (c) 2015 Paul Agron. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DetailViewController : NSViewController
+
+@end
---
+++ AcronymLookup/AcronymLookup/DetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.m
+//  AcronymLookup
+//
+//  Created by Paul Agron on 4/10/15.
+//  Copyright (c) 2015 Paul Agron. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
